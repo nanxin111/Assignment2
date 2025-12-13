83b20ab584a6b1eb1f9e9348764d71690b3c48af
karenchen <karenchen@localhost>
2025-12-13 11:37:41 -0500

    Karen

--- src/ResultFrame.java
+++ src/ResultFrame.java
@@ -91,7 +91,7 @@ public class ResultFrame extends javax.swing.JFrame {
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
-        new HomeFrame().setVisible(ture);
+        new HomeFrame().setVisible(true);
         this.dispose();
     }//GEN-LAST:event_jButton1ActionPerformed
 
